209676808b961d5fe55bef8a3ef04677203059fe
Guus Goossens <guus.goossens@gmail.com>
2013-04-10 12:04:08 +0200

    Copy en Watch grunt tasks.

--- gruntfile.js
+++ gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		typescript: {
-			base: {
+			sources: {
 				src: ['src/*.ts'],
 				dest: 'build',
 				options: {
@@ -10,11 +10,26 @@ module.exports = function(grunt) {
 					sourcemap: true
 				}
 			}
+		},
+
+		copy: {
+			sources: {
+				files: [{expand: true, cwd: 'src/', src: ['*.ts'], dest: 'build/'}]
+			}
+		},
+
+		watch: {
+			sources: {
+				files: ['src/*.ts'],
+				tasks: ['default']
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-typescript');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['typescript']);
+	grunt.registerTask('default', ['typescript', 'copy']);
 
 };
