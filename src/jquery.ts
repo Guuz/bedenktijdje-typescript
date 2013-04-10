9af40d81b3fbe54e588a6a9bb238129b1e911ca9
Guus Goossens <guus.goossens@gmail.com>
2013-04-10 17:03:27 +0200

    jQuery voorbeeld uitbreiden.

--- src/jquery.ts
+++ src/jquery.ts
@@ -2,6 +2,42 @@
 
 (function() {
 
-	$('body').hasClass('x');
+	var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+
+	var jqXHR = $.getJSON( flickerAPI, {
+		tags: "landscape",
+		tagmode: "any",
+		format: "json"
+	});
+
+	jqXHR.done(function( data ) {
+		$.each( data.items, function( i, item ) {
+
+			var $img = $('<img>')
+				.hide()
+				.attr( "src", item.media.m )
+				.appendTo('body');
+
+			$img.on('load', function() {
+				$(this).fadeIn();
+			});
+
+		});
+	});
+
+	// jqXHR.done( ( data ) => {
+	// 	$.each( data.items, ( i, item ) => {
+
+	// 		var $img = $('<img>')
+	// 			.hide()
+	// 			.attr( "src", item.media.m )
+	// 			.appendTo('body');
+
+	// 		$img.on('load', function() {
+	// 			$(this).fadeIn();
+	// 		});
+
+	// 	});
+	// });
 
 }());
