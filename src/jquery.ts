5d1db69f72779aa27d89a10e8cdce40461f36b4d
Guus Goossens <guus.goossens@gmail.com>
2013-04-11 13:36:13 +0200

    Slimmere manier van callbacks.

--- src/jquery.ts
+++ src/jquery.ts
@@ -1,9 +1,9 @@
 ///<reference path="../d.ts/jquery.d.ts"/>
 
 module demo.jquery {
-	var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+	var flickerAPI = 'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
 
-	export function showSomeImages( callback:Function ) {
+	export function showSomeImages( callback?:Function = $.noop ) {
 		var jqXHR = startRequest();
 
 		jqXHR.done(function( data ) {
@@ -11,21 +11,23 @@ module demo.jquery {
 
 				var $img = $('<img>')
 					.css('opacity', 0)
-					.attr( "src", item.media.m )
+					.attr( 'src', item.media.m )
 					.appendTo('body');
 
 				showImageWhenLoaded( $img );
 			});
 		});
+
+		jqXHR.always( callback );
 	}
 
 
 
 	function startRequest() {
 		return $.getJSON( flickerAPI, {
-			tags: "landscape",
-			tagmode: "any",
-			format: "json"
+			tags: 'landscape',
+			tagmode: 'any',
+			format: 'json'
 		});
 	}
 
