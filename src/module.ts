bfe669a0b4fc8790defb57c1a5941480fbbb218f
Guus Goossens <guus.goossens@gmail.com>
2013-04-18 11:24:29 +0200

    Voorbeeld module klaarzetten.

--- src/module.ts
+++ src/module.ts
@@ -1,9 +1,10 @@
 module demo {
 
-  var message = 'Hello, World';
+	var message = 'Hello, World';
+	//export var message = 'Hello, World';
 
 	export function sayHello() {
-		console.log(message);
+		console.log( message );
 	}
 
 }
